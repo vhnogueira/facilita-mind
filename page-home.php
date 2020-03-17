76fe3184bf0c97b914221d6a206f291ddf4556d5
Victor Hugo Nogueira de Castro <nogueira.14@gmail.com>
2020-03-17 19:50:57 -0300

    home page

--- page-home.php
+++ page-home.php
@@ -16,7 +16,7 @@ get_header(); ?>
 <div class="py-5 bg-primary min-vh-75" id="home-1">
     <div class="container">
         <div class="row">
-            <div class="col-md-7 text-light lead">
+            <div class="col-md-8 text-light lead">
                 <?php the_field('text_banner') ?>
             </div>
         </div>
@@ -31,8 +31,8 @@ get_header(); ?>
             </div>
             <div class="col-8 align-self-center">
                 <p class="h2">SOBRE MIM</p>
-                <p class="h3">JULIANA AMARAL</p>
-                <div class="lead mb-4">
+                <p class="h3 mb-4 lato text-danger font-weight-bold">JULIANA AMARAL</p>
+                <div class="mb-4">
                     <?php the_field('text_sobre') ?>
                 </div>
             </div>  
@@ -51,16 +51,35 @@ get_header(); ?>
 <div class="py-5 bg-danger" id="home-4">
     <div class="container">
         <p class="h1 text-uppercase text-center mb-4">Como posso te ajudar</p>
-        <div class="row">
-            <div class="col-md-4">
+        <div class="row align-items-center">
+            
+            <?php 
+            // the query
+            $the_query = new WP_Query( array(
+                'post_type' => 'servicos',
+                'posts_per_page' => -1,
+            )); 
+            ?>
 
-            </div>
-            <div class="col-md-4">
+            <?php if ( $the_query->have_posts() ) : ?>
+            <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 
-            </div>
-            <div class="col-md-4">
-                
-            </div>
+                <div class="col-md-4">
+                    
+                    <?php the_title(); ?>
+
+                    <?php the_field('subtitulo'); ?>
+                    <?php the_field('icone'); ?>
+                    <?php the_field('texto_botao'); ?>
+                    <?php the_field('imagem'); ?>
+
+                </div>
+
+            <?php endwhile; ?>
+            <?php wp_reset_postdata(); ?>
+
+            <?php endif; ?>
+
         </div>
     </div>
 </div>
