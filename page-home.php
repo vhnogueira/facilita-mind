f99d987138ea4227f69cdd6761d604a6a3367a35
Victor Hugo Nogueira de Castro <nogueira.14@gmail.com>
2020-03-17 15:03:09 -0300

    logo center

---
+++ page-home.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Template Name: Home Page Template
+ *
+ * Template for displaying a page just with the header and footer area and a "naked" content area in between.
+ * Good for landingpages and other types of pages where you want to add a lot of custom markup.
+ *
+ * @package understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header(); ?>
+
+<div class="py-5" id="home-1">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php get_footer();
