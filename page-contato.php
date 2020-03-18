26fa8f951b280901d09aa4bb43e3168a5e82921c
Victor Hugo Nogueira de Castro <nogueira.14@gmail.com>
2020-03-18 18:13:06 -0300

    página de contato

---
+++ page-contato.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Template Name: Página de Contato - Facilita Mind
+ *
+ * Template for displaying a page just with the header and footer area and a "naked" content area in between.
+ * Good for landingpages and other types of pages where you want to add a lot of custom markup.
+ *
+ * @package understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header(); ?>
+
+<ul>
+    <li>Teste 1</li>
+    <li>Teste 1</li>
+    <li>Teste 1</li>
+    <li>Teste 1</li>
+    <li>Teste 1</li>
+</ul>
+
+<?php get_footer();
