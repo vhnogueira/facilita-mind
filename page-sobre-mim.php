eca0bba29506efc3b813e04b283a2f741a065d1e
Victor Hugo Nogueira de Castro <nogueira.14@gmail.com>
2020-03-17 21:43:18 -0300

    page sobre

--- page-sobre-mim.php
+++ page-sobre-mim.php
@@ -1,7 +1,9 @@
 <?php
 /**
- * Template Name: Sobre - Facilita Mind
+ * Template Name: Sobre Template
  *
+ * Template for displaying a page just with the header and footer area and a "naked" content area in between.
+ * Good for landingpages and other types of pages where you want to add a lot of custom markup.
  *
  * @package understrap
  */
