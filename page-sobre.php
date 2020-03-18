41d0e712d9e5076529b2e67483f106c075b81b15
Victor Hugo Nogueira de Castro <nogueira.14@gmail.com>
2020-03-17 21:41:32 -0300

    page sobre

---
+++ page-sobre.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Template Name: Sobre - Facilita Mind
+ *
+ *
+ * @package understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header(); ?>
+
+<ul>
+    <li>Teste 1</li>
+    <li>Teste 1</li>
+    <li>Teste 1</li>
+    <li>Teste 1</li>
+    <li>Teste 1</li>
+</ul>
+
+<?php get_footer();
