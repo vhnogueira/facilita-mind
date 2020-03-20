59ba29603a3c2f142deeb5046e1328ed638d6a82
Victor Hugo Nogueira de Castro <nogueira.14@gmail.com>
2020-03-20 15:29:04 -0300

    paginas como ajudar

--- single-post-consultoria.php
+++ single-post-consultoria.php
@@ -3,7 +3,56 @@
  * Template Name: Consultoria - Template de Serviço
  * Template Post Type: servicos
  */
-  
+ 
  get_header();  ?>
+
+ <div class="py-5">
+    <div class="container">
+        <p class="h2 text-uppercase text-center">Como posso te ajudar</p>
+        <div class="row">
+
+        </div>
+    </div>
+ </div>
+
+<div class="py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <p class="h3"><?php the_field('titulo_o_que_e'); ?></p>
+                <?php the_field('o_que_e'); ?>
+            </div>
+            <div class="col-md-6">
+                <p class="h3"><?php the_field('titulo_vantagens'); ?></p>
+                <?php the_field('vantagens'); ?>
+            </div>
+            <div class="col-md-6">
+                <p class="h3"><?php the_field('titulo_destina'); ?></p>
+                <?php the_field('destina'); ?>
+            </div>
+            <div class="col-md-6">
+                <p class="h3"><?php the_field('titulo_como_acontece'); ?></p>
+                <?php the_field('como_acontece'); ?>
+            </div>
+            <div class="col-md-6 order-md-3">
+                <?php echo wp_get_attachment_image( get_field('imagem_consultoria'), 'full', "", array( "class" => "img-fluid mb-3" ) );  ?>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<div class="py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 offset-md-2">
+                <a class="btn btn-primary text-danger mt-4 btn-lg btn-block" href="<?php the_field('link_cta'); ?>"><?php the_field('texto_cta'); ?></a>
+            </div>
+        </div>
+    </div>
+</div>
+
+
 
  <?php get_footer();
--- single-post-palestras.php
+++ single-post-palestras.php
@@ -3,7 +3,45 @@
  * Template Name: Palestras - Template de Serviço
  * Template Post Type: servicos
  */
-  
+ 
  get_header();  ?>
+
+ <div class="py-5">
+    <div class="container">
+        <p class="h2 text-uppercase text-center">Como posso te ajudar</p>
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="text-center">Palestras</h1>
+            </div>
+        </div>
+    </div>
+ </div>
+
+<div class="py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 align-self-center">
+                <?php echo wp_get_attachment_image( get_field('imagem_mentoria'), 'full', "", array( "class" => "img-fluid mb-3" ) );  ?>
+            </div>
+            <div class="col-md-6 align-self-center">
+                <?php the_field('texto_mentoria'); ?>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<div class="py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 offset-md-2">
+                <a class="btn btn-primary text-danger mt-4 btn-lg btn-block" href="<?php the_field('link_cta'); ?>"><?php the_field('texto_cta'); ?></a>
+            </div>
+        </div>
+    </div>
+</div>
+
+
 
  <?php get_footer();
